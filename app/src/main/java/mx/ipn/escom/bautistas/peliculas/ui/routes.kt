902bfd7f7b523c53a52e8a46b757c9043b5c2c45
Samuel Bautista <samuelbaubas@gmail.com>
2023-05-13 18:51:12 -0600

    Refactor routes to load id from home

--- app/src/main/java/mx/ipn/escom/bautistas/peliculas/ui/routes.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/peliculas/ui/routes.kt
@@ -1,7 +1,10 @@
 package mx.ipn.escom.bautistas.peliculas.ui
 
-enum class PeliculasRoutes(){
-  SplashScreen(),
-  Home()
-}
+import androidx.annotation.StringRes
+import mx.ipn.escom.bautistas.peliculas.R
 
+sealed class Routes(val route: String, @StringRes val title: Int) {
+  object SplashScreen : Routes("SplashScreen", R.string.splash_screen)
+  object Home : Routes("Home",R.string.app_name)
+  object Details : Routes("Details",R.string.details)
+}
