3334e117e68fb6fea0691212f5344baac5b706ba
Samuel Bautista <samuelbaubas@gmail.com>
2023-05-13 18:49:33 -0600

    Refactor size and titles in MovieCard

--- app/src/main/java/mx/ipn/escom/bautistas/peliculas/ui/utils/MovieCard.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/peliculas/ui/utils/MovieCard.kt
@@ -1,37 +1,62 @@
 package mx.ipn.escom.bautistas.peliculas.ui.utils
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import mx.ipn.escom.bautistas.peliculas.R
+import mx.ipn.escom.bautistas.peliculas.model.Pelicula
 
 @Composable
 fun MovieCard(
     modifier: Modifier = Modifier,
+    pelicula: Pelicula,
+    goToDetails:(Int)->Unit
 ) {
-    Column(horizontalAlignment =Alignment.CenterHorizontally) {
+    Column(
+        modifier = modifier
+            .width(200.dp)
+            .height(330.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         ElevatedCard() {
-            Image(
-                painter = painterResource(R.drawable.ic_launcher_foreground),
-                contentDescription = "Hola",
+            AsyncImage(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(250.dp),
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data("https://image.tmdb.org/t/p/w500/${pelicula.backdropPath}")
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                error = painterResource(id = R.drawable.ic_launcher_foreground),
+                placeholder = painterResource(id = R.drawable.loading_img)
             )
-            Text(text = "Puntuacion")
+            Text(text = "${pelicula.voteAverage}")
         }
-        TextButton(onClick = { /*TODO*/ }) {
-            Text(text = "Hola")
+        TextButton(onClick = {
+            goToDetails(pelicula.id)
+        }) {
+            Column(modifier.fillMaxWidth(), horizontalAlignment = Alignment.Start) {
+                Text(
+                    text = pelicula.title,
+                    modifier = modifier.align(Alignment.Start)
+                )
+
+            }
         }
     }
-}
-
-@Preview
-@Composable
-fun MovieCardPreview() {
-    MovieCard()
 }
