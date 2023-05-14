ad298a3d8ff9c9644fd20e71c2cd23d8a0ea1072
Samuel Bautista <samuelbaubas@gmail.com>
2023-05-13 18:48:53 -0600

    Add PeliculasApp to MainActivity

--- app/src/main/java/mx/ipn/escom/bautistas/peliculas/MainActivity.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/peliculas/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import mx.ipn.escom.bautistas.peliculas.ui.PeliculasApp
 import mx.ipn.escom.bautistas.peliculas.ui.theme.PeliculasTheme
 
 class MainActivity : ComponentActivity() {
