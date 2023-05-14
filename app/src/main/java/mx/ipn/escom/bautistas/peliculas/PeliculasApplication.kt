dcbb66f199364eae93bff6f1c7a9583be6801cba
Samuel Bautista <samuelbaubas@gmail.com>
2023-05-14 12:44:55 -0600

    Add room to Application

--- app/src/main/java/mx/ipn/escom/bautistas/peliculas/PeliculasApplication.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/peliculas/PeliculasApplication.kt
@@ -1,13 +1,19 @@
 package mx.ipn.escom.bautistas.peliculas
 
 import android.app.Application
+import androidx.room.Room
 import mx.ipn.escom.bautistas.peliculas.data.AppContainer
 import mx.ipn.escom.bautistas.peliculas.data.DefaultAppContainer
+import mx.ipn.escom.bautistas.peliculas.local.PeliculasLocalService
 
 class PeliculasApplication: Application(){
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
         container= DefaultAppContainer()
+    }
+
+    val database: PeliculasLocalService by lazy {
+        Room.databaseBuilder(this, PeliculasLocalService::class.java,"peliculas_db").build()
     }
 }
